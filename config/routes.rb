57d9d67dea398f81956629e8d4b38b48257205fc
Prima Aulia Gusta <primAulia@gmail.com>
2020-05-30 10:54:26 +0800

    devise auth setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#home' # default routes after authentication
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
